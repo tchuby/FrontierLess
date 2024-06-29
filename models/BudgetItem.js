4323b4f342337b4dfb4f8785d68bc3d17df4186c
Daniel Andrade Silva <danielandradesilva.84@gmail.com>
2024-06-29 16:35:58 -0300

    Criando models

---
+++ models/BudgetItem.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize')
+const db = require('../db/connection')
+const Project = require('./Project')
+
+const BudgetItem = db.define('BudgetItem', {
+    name: {
+        type: DataTypes.STRING,
+        required: true
+    },
+    custo: {
+        type: DataTypes.DECIMAL
+    },
+    description: {
+        type: DataTypes.STRING
+    }
+})
+
+BudgetItem.belongsTo(Project, { foreignKey: { allowNull: false }, onDelete: 'CASCADE' })
+
+module.exports = BudgetItem
