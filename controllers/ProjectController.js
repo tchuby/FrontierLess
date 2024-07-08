547e475cf94b868ad8a4c3ad08396281beeb1709
Luan <luangrossert@gmail.com>
2024-07-08 02:00:21 -0300

    Atualização dos layouts

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -68,11 +68,11 @@ module.exports = class ProjectController {
     }
 
     static async createProject(req, res) {
-        const { destination, exchangeType } = req.body;
+        const { destination, exchangeType, status } = req.body;
 
         const newProject = {
             destination,
-            status: 'progredindo',
+            status,
             exchangeType,
             UserId: req.session.userid
         };
