837e725db0ea231feae654b759064a77b5cdaf0c
DANIEL ANDRADE SILVA <daniel_silva37@estudante.sesisenai.org.br>
2024-10-15 18:25:39 -0300

    ajustes profileController

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -1,52 +1,49 @@
-const Project = require('../models/Project')
-const User = require('../models/User')
+const Project = require('../models/Project');
+const User = require('../models/User');
 
 module.exports = class ProfileController {
 
     static async showProfile(req, res) {
-
         try {
             const userProfile = await User.findOne({ 
                 where: { id: req.session.userid },
                 raw: true 
             });
 
-            
             if (!userProfile) {
-                // Tratar o caso onde o usuário não é encontrado
                 return res.status(404).send('User not found');
             }
-            console.log(userProfile)
-            
-            
+
             const userProjects = await Project.findAll({ 
                 where: { UserId: req.session.userid }, 
-                raw: true
+                raw: true 
             });
-            console.log(userProjects);
-    
-            req.session.save(() => {
-                res.render('profile/profile', { userProfile, userProjects });
-            })
-    
+
+            // Retorna o perfil do usuário e os projetos em um objeto JSON
+            return res.status(200).send({ userProfile, userProjects });
+
         } catch (error) {
             console.error(error);
-            res.status(500).send('Server Error');
+            return res.status(500).send('Server Error');
         }
-        
     }
 
     static async createProject(req, res) {
-        const project = {
-            destination: req.body.destination,
-            status: req.body.status,
-            exchangeType: req.body.exchangeType,
-            UserId: req.session.userid
+        try {
+            const project = {
+                destination: req.body.destination,
+                status: req.body.status,
+                exchangeType: req.body.exchangeType,
+                UserId: req.session.userid
+            };
+
+            await Project.create(project);
+
+            // Retornar o novo projeto criado com um status 201
+            return res.status(201).send('Projeto criado com sucesso');
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send('Erro ao criar projeto');
         }
-
-        await Project.create(project)
-
-        res.redirect(`/profile/${req.session.userid}`)
     }
-
 }
